3df9987cd92046ce19cd414b93afe6aac1d2b094
Joshua Green <greenjoshua64@gmail.com>
2020-06-17 14:20:51 -0500

    Initial commit

---
+++ app/src/main/java/edu/xula/myfirstapp/MainActivity.java
@@ -0,0 +1,23 @@
+package edu.xula.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view){
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        Button button = (Button) findViewById(R.id.button);
+        button.setText(message);
+    }
+}
